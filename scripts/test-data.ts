c285a57047beddc1e552dcdf27b9350e1b1c1ce6
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2024-05-11 23:53:30 +0700

    fix test

--- scripts/test-data.ts
+++ scripts/test-data.ts
@@ -1,6 +1,6 @@
-import { parse, type ParseResult } from "../src";
-import mods from "../data/all-mods.json";
+import { parse } from "../src";
+import mods from "../test/data/all-mods.json";
 
 const output = Object.fromEntries(Object.keys(mods).map((m) => [m, parse(m)]));
 
-Bun.write("data/parse-results.json", JSON.stringify(output, undefined, 2));
+Bun.write("test/data/parse-results.json", JSON.stringify(output, undefined, 2));
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,9 +1,9 @@
 import { expect, test } from "bun:test";
-import mods from "../data/all-mods.json";
+import mods from "./data/parse-results.json";
 import { parse } from "../src";
 
 test("all mods", () => {
   for (const e of Object.entries(mods)) {
-    expect(parse(e[0]).map((v) => v.text)).toEqual(e[1]);
+    expect(parse(e[0])).toEqual(e[1]);
   }
 });
