a8b1aacff8ac4d3002afe312c346ebc22307e400
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2024-05-26 10:43:44 +0700

    fix action

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig, isFileServingAllowed } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/poestat",
+  build: {
+    target: "ES2022"
+  },
   server: {
     fs: {
       allow: [".."],
